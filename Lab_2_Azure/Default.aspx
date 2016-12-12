fe031c64c00d73532905147da9d6a34f5f5ff157
Roman Lazurak <roman.lazurak@vakoms.com.ua>
2016-12-12 21:51:41 +0200

    Add project files.

---
+++ Lab_2_Azure/Default.aspx
@@ -0,0 +1,42 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="Lab_2_Azure.Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+    <link rel="stylesheet" type="text/css" href="style.css" />
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div class="calc-container">
+        <h1>CALCULATOR</h1>
+        <div>
+            <asp:TextBox ID="CalcField" runat="server">0</asp:TextBox>
+        </div>
+        <div class="bottom-section">
+            <div class="numbers-section">
+                <asp:Button ID="btn7" runat="server" Text="7" OnClick="btnNumb7_Click" />
+                <asp:Button ID="btn8" runat="server" Text="8" OnClick="btnNumb8_Click" />
+                <asp:Button ID="btn9" runat="server" Text="9" OnClick="btnNumb9_Click" />
+                <asp:Button ID="btnAdd" runat="server" Text="+" OnClick="btnAdd_Click" />
+                <asp:Button ID="btn4" runat="server" Text="4" OnClick="btnNumb4_Click" />
+                <asp:Button ID="btn5" runat="server" Text="5" OnClick="btnNumb5_Click" />
+                <asp:Button ID="btn6" runat="server" Text="6" OnClick="btnNumb6_Click" />
+                <asp:Button ID="btnSubtract" runat="server" Text="-"  OnClick="btnSubtract_Click"/>
+                <asp:Button ID="btn1" runat="server" Text="1" OnClick="btnNumb1_Click" />
+                <asp:Button ID="btn2" runat="server" Text="2" OnClick="btnNumb2_Click" />
+                <asp:Button ID="btn3" runat="server" Text="3" OnClick="btnNumb3_Click" />
+                <asp:Button ID="btnMultiply" runat="server" Text="*"  OnClick="btnMultiply_Click"/>  
+                <asp:Button ID="btn0" runat="server" Text="0" OnClick="btnNumb0_Click" />
+                <asp:Button ID="btnClear" runat="server" Text="C" OnClick="btnClear_Click" />
+                <asp:Button ID="btnCalculate" runat="server" Text="=" OnClick="btnCalculate_Click"/> 
+                <asp:Button ID="btnDivide" runat="server" Text="/" OnClick="btnDivide_Click"/>  
+            </div>  
+        </div>
+    </div>
+    </form>
+</body>
+
+ <script type="text/javascript" src="scripts/jquery-3.1.1.js"></script>
+</html>
---
+++ Lab_2_Azure/Default.aspx.cs
@@ -0,0 +1,169 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Lab_2_Azure
+{
+    public partial class Default : System.Web.UI.Page
+    {
+
+        public enum Operations
+        {
+            None,
+            Add,
+            Subtract,
+            Multiply,
+            Divide
+        }
+
+        private static double tempValue = 0;
+        private static bool beginLine = false;
+        private static Operations LastOperation = Operations.None;
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void buttn_click(string number)
+        {
+            if (CalcField.Text == "0" || beginLine)
+            {
+                beginLine = false;
+                CalcField.Text = number;
+            }
+            else
+            {
+                CalcField.Text += number;
+            }
+        }
+
+        protected void btnNumb0_Click(object sender, EventArgs e)
+        {
+            buttn_click(btn0.Text);
+        }
+
+        protected void btnNumb1_Click(object sender, EventArgs e)
+        {
+            buttn_click(btn1.Text);
+        }
+
+        protected void btnNumb2_Click(object sender, EventArgs e)
+        {
+            buttn_click(btn2.Text);
+        }
+
+        protected void btnNumb3_Click(object sender, EventArgs e)
+        {
+            buttn_click(btn3.Text);
+        }
+
+        protected void btnNumb4_Click(object sender, EventArgs e)
+        {
+            buttn_click(btn4.Text);
+        }
+
+        protected void btnNumb5_Click(object sender, EventArgs e)
+        {
+            buttn_click(btn5.Text);
+        }
+
+        protected void btnNumb6_Click(object sender, EventArgs e)
+        {
+            buttn_click(btn6.Text);
+        }
+
+        protected void btnNumb7_Click(object sender, EventArgs e)
+        {
+            buttn_click(btn7.Text);
+        }
+
+        protected void btnNumb8_Click(object sender, EventArgs e)
+        {
+            buttn_click(btn8.Text);
+        }
+
+        protected void btnNumb9_Click(object sender, EventArgs e)
+        {
+            buttn_click(btn9.Text);
+        }
+
+        protected void btnAdd_Click(object sender, EventArgs e)
+        {
+            var currentValue = double.Parse(CalcField.Text);
+            ProceedLastOperation(currentValue);
+
+            LastOperation = Operations.Add;
+            CalcField.Text = tempValue.ToString();
+        }
+
+        protected void btnSubtract_Click(object sender, EventArgs e)
+        {
+            var currentValue = double.Parse(CalcField.Text);
+            ProceedLastOperation(currentValue);
+
+            LastOperation = Operations.Subtract;
+            CalcField.Text = tempValue.ToString();
+        }
+
+        protected void btnMultiply_Click(object sender, EventArgs e)
+        {
+            var currentValue = double.Parse(CalcField.Text);
+            ProceedLastOperation(currentValue);
+
+            LastOperation = Operations.Multiply;
+            CalcField.Text = tempValue.ToString();
+        }
+
+        protected void btnDivide_Click(object sender, EventArgs e)
+        {
+            var currentValue = double.Parse(CalcField.Text);
+            ProceedLastOperation(currentValue);
+
+            LastOperation = Operations.Divide;
+            CalcField.Text = tempValue.ToString();
+        }
+
+        protected void btnCalculate_Click(object sender, EventArgs e)
+        {
+            var currentValue = double.Parse(CalcField.Text);
+            ProceedLastOperation(currentValue);
+
+            CalcField.Text = tempValue.ToString();
+            LastOperation = Operations.None;
+        }
+
+        private void ProceedLastOperation(double currentValue)
+        {
+            beginLine = true;
+            switch (LastOperation)
+            {
+                case Operations.None:
+                    tempValue = currentValue;
+                    break;
+                case Operations.Add:
+                    tempValue = tempValue + currentValue;
+                    break;
+                case Operations.Subtract:
+                    tempValue = tempValue - currentValue;
+                    break;
+                case Operations.Multiply:
+                    tempValue = tempValue * currentValue;
+                    break;
+                case Operations.Divide:
+                    tempValue = tempValue / currentValue;
+                    break;
+            }
+        }
+
+        protected void btnClear_Click(object sender, EventArgs e)
+        {
+            tempValue = 0;
+            LastOperation = Operations.None;
+            CalcField.Text = tempValue.ToString();            
+        }
+    }
+}
